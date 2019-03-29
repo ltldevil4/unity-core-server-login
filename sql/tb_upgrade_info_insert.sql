860be5db3b6cbab42ce192b457eaa57f0a819c76
ltldevil4 <48463591+ltldevil4@users.noreply.github.com>
2019-03-29 16:47:33 +0900

    Update tb_upgrade_info_insert.sql

--- sql/tb_upgrade_info_insert.sql
+++ sql/tb_upgrade_info_insert.sql
@@ -4,15 +4,42 @@ LIMIT 0, 1000
 
 -- Date: 2018-01-13 00:36
 */
-INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (1,'Health',1,5,10);
-INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (2,'Health',2,5,10);
-INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (3,'Health',3,5,10);
-INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (101,'Damage',1,10,20);
-INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (102,'Damage',2,10,20);
-INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (103,'Damage',3,10,20);
-INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (201,'Defense',1,2,10);
-INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (202,'Defense',2,2,10);
-INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (203,'Defense',3,2,20);
-INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (301,'Speed',1,1,5);
-INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (302,'Speed',2,1,5);
-INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (303,'Speed',3,1,5);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (1,'Health',1,5,2);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (2,'Health',2,5,4);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (3,'Health',3,5,6);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (4,'Health',4,5,8);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (5,'Health',5,5,10);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (6,'Health',6,5,12);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (7,'Health',7,5,14);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (8,'Health',8,5,16);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (9,'Health',9,5,18);
+
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (101,'Damage',1,10,2);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (102,'Damage',2,10,4);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (103,'Damage',3,10,6);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (104,'Damage',4,10,8);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (105,'Damage',5,10,10);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (106,'Damage',6,10,12);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (107,'Damage',7,10,14);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (108,'Damage',8,10,16);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (109,'Damage',9,10,18);
+
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (201,'Defense',1,2,2);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (202,'Defense',2,2,4);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (203,'Defense',3,2,6);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (204,'Defense',4,2,8);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (205,'Defense',5,2,10);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (206,'Defense',6,2,12);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (207,'Defense',7,2,14);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (208,'Defense',8,2,16);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (209,'Defense',9,2,28);
+
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (301,'Speed',1,1,2);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (302,'Speed',2,1,4);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (303,'Speed',3,1,6);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (304,'Speed',4,1,8);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (305,'Speed',5,1,10);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (306,'Speed',6,1,12);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (307,'Speed',7,1,14);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (308,'Speed',8,1,16);
+INSERT INTO `tb_upgrade_info` (`upgrade_id`,`upgrade_type`,`upgrade_level`,`upgrade_amount`,`upgrade_cost`) VALUES (309,'Speed',9,1,18);
